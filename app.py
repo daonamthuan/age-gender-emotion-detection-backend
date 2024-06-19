afdcc2c81770c3a495d46c48fd71d7b424bef3e7
DaoNamThuan <21522649@gm.uit.edu.vn>
2024-06-20 01:14:54 +0700

    add handling of webcam source with socket

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
 import cv2
 import numpy as np
-from flask import Flask, request, jsonify
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing.image import img_to_array
-from PIL import Image
 import io
-from ultralytics import YOLO
+from flask import Flask, request, jsonify
 from flask_cors import CORS
-from matplotlib import pyplot as plt
+from flask_socketio import SocketIO, emit
+from tensorflow.keras.models import load_model
+from PIL import Image
+from ultralytics import YOLO
+import base64
+
 
 app = Flask(__name__)
+# Thiết lập timeout cho các request
+app.config['PERMANENT_SESSION_LIFETIME'] = 600  # Timeout 10 phút
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 600  # Timeout 10 phút
 CORS(app)
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 # Load YOLOv8 model for face detection
 yolo_model = YOLO('yolov8n_face.pt')
@@ -27,7 +32,6 @@ emotion_ranges = ['positive', 'negative', 'neutral']
 
 def get_face_detections(image):
     if image is None or not isinstance(image, np.ndarray):
-        print("=============================Invalid Image Array================================")
         raise ValueError("Invalid image array")
     
     results = yolo_model(image)
@@ -44,17 +48,16 @@ def get_face_detections(image):
     print(face_bounding_boxes)
     return face_bounding_boxes
 
+
 @app.route('/predict', methods=['POST'])
 def predict():
     if 'file' not in request.files:
         return jsonify({"error": "No image file in request"}), 400
     
-   
     file = request.files['file']
     image = Image.open(file.stream)
     image = np.array(image)
     if image.shape[-1] == 4:
-        # Chuyển đổi từ RGBA sang RGB nếu cần thiết
         image = cv2.cvtColor(image, cv2.COLOR_RGBA2RGB)
 
     # Detect faces
@@ -64,11 +67,10 @@ def predict():
 
     predictions = []
     i = 0
-    for (x, y, w, h) in face_detections:
-        i += 1
-        
+    for (x, y, w, h) in face_detections:        
         face_img = gray_image[y:y+h, x:x+w]
 
+        # sửa lại preprocessing chỗ này
         emotion_img = cv2.resize(face_img, (48, 48), interpolation = cv2.INTER_AREA)
         emotion_image_array = np.array(emotion_img)
         emotion_input = np.expand_dims(emotion_image_array, axis=0)
@@ -93,11 +95,52 @@ def predict():
             'gender': output_gender,
             'emotion': output_emotion
         })
-        
-        output_str = f"{i}: {output_gender}, {output_age}, {output_emotion}"
-        print(output_str)
 
     return jsonify(predictions)
+
+
+@socketio.on('image')
+def handle_image(data):
+    image_data = data['image']
+    image = Image.open(io.BytesIO(base64.b64decode(image_data)))
+    image = np.array(image)
+    if image.shape[-1] == 4:
+        image = cv2.cvtColor(image, cv2.COLOR_RGBA2RGB)
+
+    face_detections = get_face_detections(image)
+
+    gray_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+
+    predictions = []
+    for (x, y, w, h) in face_detections:
+        face_img = gray_image[y:y+h, x:x+w]
+
+        # sửa lại preprocessing chỗ này
+        emotion_img = cv2.resize(face_img, (48, 48), interpolation=cv2.INTER_AREA)
+        emotion_image_array = np.array(emotion_img)
+        emotion_input = np.expand_dims(emotion_image_array, axis=0)
+        output_emotion = emotion_ranges[np.argmax(emotion_model.predict(emotion_input))]
+
+        gender_img = cv2.resize(face_img, (100, 100), interpolation=cv2.INTER_AREA)
+        gender_image_array = np.array(gender_img)
+        gender_input = np.expand_dims(gender_image_array, axis=0)
+        output_gender = gender_ranges[np.argmax(gender_model.predict(gender_input))]
+
+        age_image = cv2.resize(face_img, (200, 200), interpolation=cv2.INTER_AREA)
+        age_input = age_image.reshape(-1, 200, 200, 1)
+        output_age = age_ranges[np.argmax(age_model.predict(age_input))]
+
+        predictions.append({
+            'x': x,
+            'y': y,
+            'width': w,
+            'height': h,
+            'age': output_age,
+            'gender': output_gender,
+            'emotion': output_emotion
+        })
+
+    emit('prediction', predictions)
 
 if __name__ == '__main__':
     app.run(debug=True)
